c2193e2044c3088f0d7b0c8dc2b53ba15fa5889c
caesars335 <6131302003@lamduan.mfu.ac.th>
2020-03-13 14:37:50 +0700

    serialization cookie and session

--- app.js
+++ app.js
@@ -2,25 +2,38 @@ const express = require("express");
 const path = require("path");
 const authRoutes = require("./routes/auth-routes");
 const passportSetup = require("./config/passport-setup");
-
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+const key = require("./config/key");
 const app = express();
 
 
 app.set("view engine", "ejs");
 
 //============= Middleware ===============
+//cookie
+app.use(cookieSession({
+    maxAge: 1000*60*60,
+    keys: [key.cookie.secret]
+}));
+// init passport for se/derialization
+app.use(passport.initialize());
+// session
+app.use(passport.session());
 // authen
-app.use("/auth" , authRoutes);
+app.use("/auth", authRoutes);
+
+
 
 
 // ======== Root Service ========
-app.get("/", (req,res) =>{
+app.get("/", (req, res) => {
     // res.sendFile(path.join(__dirname,"views/home.html"));
     res.render("home.ejs");
 });
 
-const PORT = 3000;
+const PORT = 35000;
 
-app.listen(PORT,() =>{
+app.listen(PORT, () => {
     console.log("Server is running at " + PORT)
 });
