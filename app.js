5b59eed3a13290187da63cbd1aa68a40eb1b3724
caesars335 <6131302003@lamduan.mfu.ac.th>
2020-03-13 15:29:32 +0700

    profile page , authCheck

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const authRoutes = require("./routes/auth-routes");
+const profileRoutes = require("./routes/profile-routes");
 const passportSetup = require("./config/passport-setup");
 const passport = require("passport");
 const cookieSession = require("cookie-session");
@@ -22,6 +23,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 // authen
 app.use("/auth", authRoutes);
+// profle
+app.use("/profile", profileRoutes);
 
 
 
